26582d00626b7ec0a22993b3b94c2885682b4f5a
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-06-08 22:28:57 -0500

    Connect with back basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+export default class Orders extends React.Component {
+  state = {
+    orders: []
+  }
+
+  async componentDidMount() {
+    const response = await axios.get(`http://127.0.0.1:3000/api/v1/orders`)
+    const orders = response.data.orders;
+    this.setState({ orders });
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.orders.map(order => <li>{order.id}</li>)}
+      </ul>
+    )
+  }
 }
-
-export default App;
